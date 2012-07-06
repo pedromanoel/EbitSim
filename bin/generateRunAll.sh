ecd8b30ce50fc820fbb5e876945c17e7ea166274
Pedro Manoel Fabiano Alves Evangelista <pedro.evangelista@gmail.com>
2012-07-06 19:00:53 -0300

    Make a different parameter test.

--- bin/generateRunAll.sh
+++ bin/generateRunAll.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+if [[ $# -lt 1 || $# -gt 2 ]]; then
+    echo "Usage: $0 <runs> [CONFIG]"
+    exit 1
+fi
 
 EXEC_FILE="../src/EbitSim" # path to the simulation executable
 INI_FILE="../simulations/BasicConfig.ini" # path to the configuration file
-CONFIG="BasicTopology" # name of the configuration
-LOG_DIR="./logs"
-
-if [[ $# != 1 ]]; then
-    echo "Usage: $0 <count>"
-    exit 1
+if [[ $# == 2 ]]; then
+    CONFIG=$2
+else
+    CONFIG="SimpleTopology" # name of the configuration
 fi
+LOG_DIR="./logs"
 
 declare -i num_runs=$1
 
@@ -27,5 +30,5 @@ echo
 
 for (( i=0; i < $num_runs; i++ ))
 do
-    echo -e "r$i:\n\t./Cmdenv $i
+    echo -e "r$i:\n\t./Cmdenv $i $CONFIG"
 done
